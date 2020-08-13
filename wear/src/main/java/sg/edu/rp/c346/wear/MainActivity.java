8b1e5ce9179544579998d76f436cc2dc464d4a20
18050268 <mikunation01>
2020-08-13 13:07:07 +0800

    Initial commit + Completion

---
+++ wear/src/main/java/sg/edu/rp/c346/wear/MainActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c346.wear;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+    private Button btnSpeak;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
